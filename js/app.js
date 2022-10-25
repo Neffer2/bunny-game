62aa31e070d82e3bd4bdcdc1fae218199f8e310c
N3f73r <n3f73r@gmail.com>
2022-10-24 23:32:16 -0500

    TileMap (por corregir)

--- js/app.js
+++ js/app.js
@@ -5,10 +5,32 @@ class MainScene extends Phaser.Scene {
     } 
 
     preload(){
+        /* Tilemap */
+            this.load.image('gameTiles', './assets/img/tilemap.png');  
+            this.load.tilemapTiledJSON('tilemap', './assets/img/tilemap-bunnygame.json');
+        /* --- */
+        
+        //NOTA: Para sacar el frameWidth y el frameHeight se divide el tamaño de la imagen sobre el numero de obj
+        this.load.spritesheet('cars', './assets/img/cars.png', {frameWidth: 140, frameHeight: 70});
+        this.load.spritesheet('cars2', './assets/img/cars2.png', {frameWidth: 150, frameHeight: 90});
+        this.load.spritesheet('bigvehicles', './assets/img/bigvehicles.png', {frameWidth: 250, frameHeight: 100.25});
+        this.load.spritesheet('motorcycles', './assets/img/motorcycle.png', {frameWidth: 158, frameHeight: 62.5});
+        this.load.spritesheet('rabbit', './assets/img/rabbits.png', {frameWidth: 40, frameHeight: 40});
     }
  
     create(){
-
+        // Añadiendo el TileMap 
+        /*
+            Se crea la constante map que inicia un tilemap
+            Se asocia la imagen con el JSON (Tilemap es el nombre que está dentro del JSON)
+            Se imprimen las capas
+        */
+        const map = this.add.tilemap('tilemap');
+        const tileset = map.addTilesetImage('Tilemap', 'gameTiles');
+        map.createLayer('Grass', tileset);
+        map.createLayer('YellowLine', tileset);
+        map.createLayer('GrayRace', tileset);
+        map.createLayer('WhiteLine', tileset);
     }
 
     update(){
